f16676d774fc69657504758818ef9278f6d76106
Ryan Thompson <128102031+CaptainRyan47@users.noreply.github.com>
2023-07-07 16:19:23 -0700

    Add gameState module and playerFactory

--- script.js
+++ script.js
@@ -5,8 +5,30 @@ const gameBoard = (() => {
       console.log(cell)
     })
     gameBoard.push(cell)
-  }) 
+  })
   return { gameBoard }
 })()
 
-console.log(gameBoard)
+const gameState = (() => {
+  let turnCounter = 0
+  let players = []
+  const addPlayer = (player) => {
+    players.push(player)
+  }
+  const getActivePlayer = () => {
+    if (turnCounter % 2 === 0) return players[0]
+    else return players[1]
+  }
+  const checkForWinner = () => { }
+  return { turnCounter, checkForWinner, addPlayer, getActivePlayer }
+})()
+
+const playerFactory = (player, marker) => {
+  const placeMarker = () => { console.log(`${player} plays ${marker}`) }
+  return { player, marker, placeMarker }
+}
+
+const player1 = playerFactory('player1', 'X')
+gameState.addPlayer(player1)
+const player2 = playerFactory('player2', 'O')
+gameState.addPlayer(player2)
