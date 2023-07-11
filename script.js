42e306652fed0a0d2cfd07b88ec0bd2fe04f166b
Ryan Thompson <128102031+CaptainRyan47@users.noreply.github.com>
2023-07-11 12:52:19 -0700

    Add function to check if any player has won

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const gameBoard = (() => {
   let markerArray = [, , , , , , , ,]
+  const makeMarkerElement = (marker) => {
+    let markerElement = document.createElement('p')
+    markerElement.textContent = marker
+    return markerElement
+  }
   document.querySelectorAll('.cell').forEach(cell => {
     cell.addEventListener('click', () => {
-      cell.append(gameState.getActivePlayer().getMarkerElement())
-      gameBoard[cell.id.charAt(4)] = gameState.getActivePlayer().marker
-      gameState.checkForWinner()
+      if (cell.querySelector('p') != null) return
+      cell.append(makeMarkerElement(gameState.getActivePlayer().marker))
+      markerArray[cell.id.charAt(4)] = gameState.getActivePlayer().marker
+      gameState.checkForWinner(markerArray)
       gameState.incrementTurnCounter()
     })
   })
@@ -24,17 +30,22 @@ const gameState = (() => {
     if (turnCounter % 2 === 0) return players[0]
     else return players[1]
   }
-  const checkForWinner = () => { }
+  const checkForWinner = (array) => {
+    if (array[0] && array[0] === array[1] && array[0] === array[2]) getActivePlayer().winner = true
+    if (array[0] && array[0] === array[4] && array[0] === array[8]) getActivePlayer().winner = true
+    if (array[0] && array[0] === array[3] && array[0] === array[6]) getActivePlayer().winner = true
+    if (array[1] && array[1] === array[4] && array[1] === array[7]) getActivePlayer().winner = true
+    if (array[2] && array[2] === array[5] && array[2] === array[8]) getActivePlayer().winner = true
+    if (array[3] && array[3] === array[4] && array[3] === array[5]) getActivePlayer().winner = true
+    if (array[6] && array[6] === array[7] && array[6] === array[8]) getActivePlayer().winner = true
+    if (array[6] && array[6] === array[4] && array[6] === array[2]) getActivePlayer().winner = true
+  }
   return { turnCounter, checkForWinner, addPlayer, getActivePlayer, incrementTurnCounter }
 })()
 
 const playerFactory = (player, marker) => {
-  const getMarkerElement = () => {
-    let markerElement = document.createElement('p')
-    markerElement.textContent = marker
-    return markerElement
-  }
-  return { player, getMarkerElement, marker }
+  let winner = false
+  return { player, marker, winner }
 }
 
 const player1 = playerFactory('player1', 'X')
